f9b53ea41b96bd4aeb95e68403e89534ac14f2d4
Eduardo Gómez Manzano <oe.gomez.m@proton.me>
2026-08-21 13:28:00 -0600

    Mis actualizaciones de proyecto del Sprint 8

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 
 # Inserta tu dirección de URL sin la barra diagonal al final
-URL_SERVICE = "https://cnt-f20b0c1e-5137-4ecb-a056-6bde7fa0bd68.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-63b18240-0fbb-4de9-953c-1ebe09f75959.containerhub.tripleten-services.com"
 
 # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_USER_PATH ="/api/v1/users/"
